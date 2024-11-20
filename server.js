03b51e840a24b133785d8fee26f6000a111e8ef5
codeManifest <147244025+codeManifest@users.noreply.github.com>
2024-11-20 21:03:26 +0530

    server ready

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express= require('express')
 const bodyparser=require('body-parser')
 const authRoutes=require('./routes/authRoutes')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 
 const MONGO_URI = 'mongodb+srv://nabins9678:UU5d4NCdpIQapyIs@education.naznvfp.mongodb.net/?retryWrites=true&w=majority&appName=education'
+app.use(cors({
+    origin: 'http://localhost:3000', // Allow only this origin
+    // methods: 'GET,POST,PUT,DELETE', 
+    credentials: true // Allow cookies or credentials if needed
+}));
 const app= express()
 const PORT = 5000;
 
